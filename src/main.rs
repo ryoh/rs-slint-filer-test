ff327b5b7571a3e32c1d2ed6020f2c2a7dc2eb4d
Ryo Kawai <kawairyoh@gmail.com>
2026-07-03 00:15:51 +0900

    feat: ファイルダイアログ機能を追加し、UIコンポーネントを更新

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use anyhow::Result;
+use log::*;
+use rfd::FileDialog;
 use std::error::Error;
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize the logger (e.g. for Slint warnings)
+    env_logger::init();
+
+    // Load the UI from the .slint file
     let ui = AppWindow::new()?;
 
-    let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+    ui.on_open_file_dialog(move || {
+        info!("Clicked Opening file dialog...");
+        let path = FileDialog::new()
+            .set_directory(std::env::home_dir().unwrap())
+            .pick_folder();
+
+        info!("Selected folder: {:?}", path);
     });
 
+    // Show the UI
     ui.run()?;
 
     Ok(())
