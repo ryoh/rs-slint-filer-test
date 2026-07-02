343b1074cc2efe542c9e6cd1dca4b18b4b8cf8c8
Ryo Kawai <kawairyoh@gmail.com>
2026-07-03 00:39:51 +0900

    feat: ファイルダイアログ機能を実装し、エントリのメタデータを取得するロジックを追加

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,41 @@
 use anyhow::Result;
 use log::*;
 use rfd::FileDialog;
+use slint::SharedString;
 use std::error::Error;
+use std::ffi::OsString;
+use std::fs;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 slint::include_modules!();
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum EntryKind {
+    File,
+    Directory,
+    Symlink,
+    Other,
+}
+
+#[derive(Debug, Clone)]
+pub struct FsEntry {
+    pub path: PathBuf,
+    pub metadata: fs::Metadata,
+    pub file_name: OsString,
+    pub kind: EntryKind,
+}
+
+pub struct EntryRow {
+    pub name: SharedString,
+    pub kind: SharedString,
+    pub size: SharedString,
+    pub modified: SharedString,
+
+    pub sort_size: u64,
+    pub sort_modified: u64,
+    pub is_directory: bool,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Initialize the logger (e.g. for Slint warnings)
@@ -22,6 +54,35 @@ fn main() -> Result<(), Box<dyn Error>> {
             .pick_folder();
 
         info!("Selected folder: {:?}", path);
+        for entry in WalkDir::new(path.unwrap())
+            .max_depth(1)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .skip(1)
+        {
+            info!("Found file: {:?}", entry.path());
+            let metadata = match fs::metadata(entry.path()) {
+                Ok(metadata) => metadata,
+                Err(e) => {
+                    error!("Failed to get metadata for file {:?}: {}", entry.path(), e);
+                    continue;
+                }
+            };
+
+            let fsentry = FsEntry {
+                path: entry.path().to_path_buf(),
+                metadata,
+                file_name: entry.file_name().to_os_string(),
+                kind: match entry.file_type() {
+                    t if t.is_file() => EntryKind::File,
+                    t if t.is_dir() => EntryKind::Directory,
+                    t if t.is_symlink() => EntryKind::Symlink,
+                    _ => EntryKind::Other,
+                },
+            };
+
+            info!("FsEntry: {:?}", fsentry);
+        }
     });
 
     // Show the UI
